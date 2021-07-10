6cf16abe235054d1005562ef94913677f066d220
Banan <bananiwo@gmail.com>
2021-07-10 17:36:52 +0200

    implement new shark movement

--- SharkMovement.cs
+++ SharkMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class SharkMovement : BasicMovement
 {
+
+    private float turnTimer = 0f;
+    private float baseTurnDuration = 2f;
+    private float turnSpeed = 0.05f;
+
+    private float angle = 0f;
+    private bool right = true;
+
     protected override void Start()
     {
         base.Start();
@@ -11,7 +19,29 @@ public class SharkMovement : BasicMovement
 
     protected override void FixedUpdate()
     {
-        base.FixedUpdate();
+////////////////////////// turn
+        turnTimer += Time.fixedDeltaTime;
+        if(turnTimer >= Random.Range(0.6f, 1.3f) * baseTurnDuration){
+            turnTimer = 0f;
+            angle = Random.Range(30f, 80f);
+            right = Random.Range(0, 2) != 1;
+            if(right){
+                angle *= -1;
+            }
+        }
+        if(turnTimer < 0.5f * baseTurnDuration * Random.Range(0.2f, 1.3f)){
+            float deltaAngle = angle * turnSpeed / baseTurnDuration;
+            m_rb.MoveRotation(m_rb.rotation + deltaAngle);
+        }
+
+////////////////////////// go forwards
+        Vector2 direction = transform.right;
+        m_rb.velocity = direction *  2f;
+
+//////////////////////////
+        //avoidObstacles();
+        //regulateSpeedMinMax();
+        // faceVelocity();
     }
 
     override protected void OnTriggerEnter2D(Collider2D other)
@@ -23,5 +53,6 @@ public class SharkMovement : BasicMovement
     {
         base.OnTriggerExit2D(other);
     }
+
 
 }
